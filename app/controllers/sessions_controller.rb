017312002ff0ef1221d983373698e84a83f31b70
itzki <itzki.h@gmail.com>
2013-06-15 23:17:56 +0900

    Implement facebook authentication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def show
+    flash.now.alert = params[:message] if params[:message]
+  end
+
+  def create
+    reset_session
+    user = User.from_omniauth(request.env['omniauth.auth'])
+    session[:user_id] = user.id
+    redirect_to root_url, notice: 'Logged in successfully'
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_url, notice: 'Logged out successfully'
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,18 @@ class User
   field :token, type: String
   field :image, type: String
   has_and_belongs_to_many :friends, class_name: 'User'
+
+  class << self
+    def from_omniauth auth
+      user = User.find_by(id: auth.uid)
+      return user if user
+      User.create({
+        name: auth.info.name,
+        email: auth.info.email,
+        id: auth.uid,
+        token: auth.credentials.token,
+        image: auth.info.image,
+      })
+    end
+  end
 end
