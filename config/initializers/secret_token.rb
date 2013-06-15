f4ab46aaacbc48b61a9f2dbec6b18258d8eff537
itzki <itzki.h@gmail.com>
2013-06-15 21:42:56 +0900

    rails new -O -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Thanksnote::Application.config.secret_key_base = 'e112b957f57b0a3131449866ed09f49e138f68fc5bf8fb903c64c8b1139517b03d29a3e735bbdbab13dfb1ca83fb53e7b214b7f76fb0f6c3987fd388acbb0cab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Thanksnote::Application.config.session_store :cookie_store, key: '_thanksnote_session'
