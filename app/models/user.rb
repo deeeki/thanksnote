ed4858bab857624a5d293ddea553171d7648369b
itzki <itzki.h@gmail.com>
2013-06-16 00:14:20 +0900

    Implement friends registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,22 +2,48 @@ class User
   include Mongoid::Document
   include Mongoid::Timestamps
   field :name, type: String
+  field :image, type: String
   field :email, type: String
   field :token, type: String
-  field :image, type: String
   has_and_belongs_to_many :friends, class_name: 'User'
 
   class << self
     def from_omniauth auth
-      user = User.find_by(id: auth.uid)
-      return user if user
+      if user = User.find_by(id: auth.uid)
+        unless user.email?
+          user.update_attributes({
+            email: auth.info.email,
+            token: auth.credentials.token,
+          })
+        end
+        user
+      else
+        User.create({
+          id: auth.uid,
+          name: auth.info.name,
+          image: auth.info.image,
+          email: auth.info.email,
+          token: auth.credentials.token,
+        })
+      end
+    end
+  end
+
+  def client
+    @client ||= FbGraph::User.me(token)
+  end
+
+  def register_friends
+    friend_ids = []
+    client.friends.each do |friend|
+      friend_ids << friend.identifier
+      next if User.find_by(id: friend.identifier)
       User.create({
-        name: auth.info.name,
-        email: auth.info.email,
-        id: auth.uid,
-        token: auth.credentials.token,
-        image: auth.info.image,
+        id: friend.identifier,
+        name: friend.name,
+        image: "http://graph.facebook.com/#{friend.identifier}/picture?type=square",
       })
     end
+    update_attribute(:friend_ids, friend_ids)
   end
 end
