af093624bd336585dccb96cda254212aab676b02
itzki <itzki.h@gmail.com>
2013-06-16 09:10:26 +0900

    rails g controller messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Thanksnote::Application.routes.draw do
-  root 'sessions#show'
+  root 'messages#index'
   get '/auth/failure' => 'sessions#show'
   get '/auth/:provider/callback' => 'sessions#create'
   get 'logout' => 'sessions#destroy'
+  resources :messages, only: [:index, :new, :create]
 end
